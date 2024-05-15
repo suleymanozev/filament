bc7cbb09bffb394835caa4db22a0528068ab068e
Süleyman ÖZEV <suleymanozev@protonmail.com>
2024-05-16 02:27:27 +0300

    Create validation.php

---
+++ packages/forms/resources/lang/tr/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'distinct' => [
+        'must_be_selected' => 'En az bir tane :attribute alanı seçilmelidir.',
+        'only_one_must_be_selected' => 'Sadece bir tane :attribute seçili olmalıdır.',
+    ],
+
+];
